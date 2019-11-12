0becba76fd412fb8da692c1217660138b4cd50f0
Tina <tina.temishova@gmail.com>
2019-11-12 13:01:48 -0600

    Main class of Project.

---
+++ src/main/java/com/restProject/SpringRestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.restProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestProjectApplication.class, args);
+	}
+
+}
